9abdb64017060e17a3c0b4c24a81e5668aa71d3c
watajun <watajun6@yahoo.co.jp>
2022-11-22 00:41:16 +0900

    add

--- server/index.php
+++ server/index.php
@@ -13,6 +13,7 @@ $dbh = connect_db();
 
     <?php include_once __DIR__ . '/_head.php' ?>
 
+
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ハッティ</title>
